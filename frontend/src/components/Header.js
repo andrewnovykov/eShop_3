c27cc614ef91dbea981bbaf91bd73ce4732832b7
Anrew <papapin777@gmail.com>
2020-10-13 20:20:11 -0700

    first commit - 7

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container,  Navbar, Nav } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+      <header>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">My Shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link href="/cart">
+                  <i className="fas fa-shopping-cart"></i>
+                  Cart
+                </Nav.Link>
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i>
+                  Sing in
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+}
+
+export default Header
+
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Product from "../components/Product";
+import products from '../products'
+
+
+function HomeScreen() {
+    return (
+        <>
+            <h1>latest products</h1>
+            <Row>
+                { products.map((product ) => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product}></Product>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
