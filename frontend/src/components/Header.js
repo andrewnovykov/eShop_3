03cd135bbf40ffa2cdec7a8aa591959bd13d8e3b
Anrew <papapin777@gmail.com>
2020-10-14 20:55:44 -0700

    adding router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import { Container,  Navbar, Nav } from 'react-bootstrap'
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
-    return (
-      <header>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand href="/">My Shop</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link href="/cart">
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>My Shop</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
                   <i className="fas fa-shopping-cart"></i>
                   Cart
                 </Nav.Link>
-                <Nav.Link href="/login">
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
                   <i className="fas fa-user"></i>
                   Sing in
                 </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-}
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;
