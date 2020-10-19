3c9d42302a833833463f8a63773843411302b554
Anrew <papapin777@gmail.com>
2020-10-18 17:33:40 -0700

    parsing data from api

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import Product from "../components/Product";
-import products from '../products'
+import Product from "../components/Product"
+import axios from 'axios'
+
 
 
 function HomeScreen() {
+    const [ products, setProducts ] = useState([])
+
+    useEffect(() => {
+      const fetchProducts = async () => {
+          const { data } =  await axios.get('/api/products')   
+          
+          setProducts(data)
+      }
+
+      fetchProducts()
+
+    },[])
+
     return (
         <>
             <h1>latest products</h1>
