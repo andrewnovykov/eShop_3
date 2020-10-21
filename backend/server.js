457d21530dd48c7565ccea8c1ded4fd0ca2b6ed1
Anrew <papapin777@gmail.com>
2020-10-20 20:35:32 -0700

    adding db connection + colors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connecDB from './config/db.js'
+import products from './data/products.js'
 
 dotenv.config()
+
+connecDB()
 
 const app = express()
 
@@ -22,4 +26,4 @@ app.get('/api/products/:id', (req, res)=> {
 
 const PORT = process.env.PORT || 5000
 
-app.listen( PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT} ` ))
+app.listen( PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT} `.yellow.bold ))
