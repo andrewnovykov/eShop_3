724fb85db8aec806c5f0768e932370a3c8d3f4dc
papapin <papapin777@gmail.com>
2020-12-07 21:11:24 -0800

    pulling data from cloud

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connecDB from './config/db.js'
-import products from './data/products.js'
+//import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
+
 
 dotenv.config()
 
@@ -14,14 +17,7 @@ app.get('/', (req, res)=> {
     res.send('API is runing')
 })
 
-app.get('/api/products', (req, res)=> {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res)=> {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use("/api/products", productRoutes);
 
 
 const PORT = process.env.PORT || 5000
